2d7510e7ab63b5eeeb4ccac11c094f91f1265089
Will Diep <willwebdesigns@gmail.com>
2021-03-07 03:14:01 -0800

    About Page - working on rich text area

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,16 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import get from "lodash/get"
-import Layout from "../components/layout"
-import Img from "gatsby-image"
-import { v4 as uuidv4 } from "uuid"
-import { graphql } from "gatsby"
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import get from 'lodash/get'
+import Layout from '../components/layout'
+import Img from 'gatsby-image'
+import { v4 as uuidv4 } from 'uuid'
+import { graphql } from 'gatsby'
+import EmailIcon from '../images/email-icon.svg'
+import InstagramIcon from '../images/instagram-icon.svg'
+import LinkedIcon from '../images/linkedin-icon.svg'
 
 function About({ data }) {
-  const siteTitle = get(this, "props.data.site.siteMetadata.title")
+  const siteTitle = get(this, 'props.data.site.siteMetadata.title')
 
   const headerText = data.allContentfulAboutPage.edges[0].node.headerText
 
@@ -31,6 +34,10 @@ function About({ data }) {
   const bottomRightImage =
     data.allContentfulAboutPage.edges[0].node.bottomRightImage.fluid
 
+  
+  
+
+
   return (
     <>
       <Layout>
@@ -43,17 +50,23 @@ function About({ data }) {
 
         {connectText}
 
-        {emailUrl}
+        <a href={emailUrl} target="_blank" rel="noreferrer">
+          <img src={EmailIcon} alt="Email Logo" />
+        </a>
 
-        {instagramUrl}
+        <a href={instagramUrl} target="_blank" rel="noreferrer">
+          <img src={InstagramIcon} alt="Email Logo" />
+        </a>
 
-        {linkedinUrl}
+        <a href={linkedinUrl} target="_blank" rel="noreferrer">
+          <img src={LinkedIcon} alt="Email Logo" />
+        </a>
 
-        <div style={{ width: "600px" }}>
+        <div style={{ width: '300px' }}>
           <Img fluid={topLeftImage} />
         </div>
 
-        <div style={{ width: "600px" }}>
+        <div style={{ width: '300px' }}>
           <Img fluid={bottomRightImage} />
         </div>
       </Layout>
