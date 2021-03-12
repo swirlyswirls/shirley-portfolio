ac78d06d3a9890dff7047002d762de75fac96b1c
Will Diep <willwebdesigns@gmail.com>
2021-03-12 03:16:06 -0800

    Work Page - change project photo and text styling based on project hover

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -6,21 +6,22 @@ const GlobalStyles = createGlobalStyle`
     --color-red: #fe3966;
     --color-black: #33241e;
     --color-gray: #8e7f79;
-    --color-link: #51d3f5;
+    --color-light-gray: #cdc1bc;
+    --color-blue: #51d3f5;
 
-    --text-60: 60px;
+    /* --text-60: 60px;
     --text-36: 36px;
     --text-30: 30px;
     --text-28: 28px;
     --text-24: 24px;
     --text-28: 28px;
     --text-18: 18px;
-    --text-16: 16px;
+    --text-16: 16px; */
 
   }
 
   .nav-active {
-    border-bottom: 3px solid var(--color-link);
+    border-bottom: 3px solid var(--color-blue);
     padding-bottom: 8px;
   }
 `;
